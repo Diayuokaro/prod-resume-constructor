120b647871f82d326cfb269854c8d980902a4125
Denis Skvortsov <diayuokaro@gmail.com>
2024-02-29 22:49:19 +0300

    Transitional Update

--- solution/sources/scripts/main.js
+++ solution/sources/scripts/main.js
@@ -1,76 +1,172 @@
 'use strict'
 
-// class ResumeController {
-//   constructor () {
+class ResumeBuilder {
+  resume = {
+    interests: [],
+    languages: [],
+    jobs: [],
+    educations: [],
+    courses: [],
+  }
 
-//   }
+  constructor () {
+    this.resume.id = self.crypto.randomUUID()
+  }
 
-//   useItems () {
-//     document.body.appendChild(document.getElementsByName('items')[0].content.cloneNode(true))
-//   }
+  setPhoto (event) {
+    const file = document.querySelector('input[type=file]').files[0]
 
-//   useResume () {
-//     document.body.appendChild(document.getElementsByName('resume')[0].content.cloneNode(true))
-//   }
+    let reader = new FileReader()
+    reader.addEventListener('load', () => {
+      this.resume.photo = reader.result
+    })
 
-//   useBuilder () {
-//     document.body.appendChild(document.getElementsByName('builder')[0].content.cloneNode(true))
-//   }
-// }
+    if (file) {
+      reader.readAsDataURL(file)
+    }
+  }
+
+  setFullname (event) {
+    this.resume.fullname = event.target.value
+  }
+
+  setBirthdate (event) {
+    this.resume.birthdate = Date(event.target.value)
+  }
+
+  setResidence (event) {
+    this.resume.residence = event.target.value
+  }
+
+  setPhone (event) {
+    this.resume.phone = event.target.value
+  }
+
+  setEmail (event) {
+    this.resume.email = event.target.value
+  }
+
+  addInterest () {
+    this.resume.interests.push({
+      interest: document.getElementsByName('interest')[0].value,
+    })
+  }
+
+  addLanguage () {
+    this.resume.languages.push({
+      languageName: document.getElementsByName('languageName')[0].value,
+      languageLevel: document.getElementsByName('languageLevel')[0].value,
+    })
+  }
+
+  setPersonalDescription (personalDescription) {
+    this.resume.personalDescription = personalDescription
+  }
+
+  addJob () {
+    this.resume.jobs.push({
+      jobTitle: document.getElementsByName('jobTitle')[0].value,
+      jobDateStart: document.getElementsByName('jobDateStart')[0].value,
+      jobDateEnd: document.getElementsByName('jobDateEnd')[0].value,
+      jobPlace: document.getElementsByName('jobPlace')[0].value,
+      jobDescription: document.getElementsByName('jobDescription')[0].value,
+    })
+  }
+
+  addEducation () {
+    this.resume.educations.push({
+      educationTitle: document.getElementsByName('educationTitle')[0].value,
+      educationDateStart: document.getElementsByName('educationDateStart')[0].value,
+      educationDateEnd: document.getElementsByName('educationDateEnd')[0].value,
+      educationPlace: document.getElementsByName('educationPlace')[0].value,
+      educationDescription: document.getElementsByName('educationDescription')[0].value,
+    })
+  }
+
+  addCourse () {
+    this.resume.courses.push({
+      courseTitle: document.getElementsByName('courseTitle')[0].value,
+      courseDateStart: document.getElementsByName('courseDateStart')[0].value,
+      courseDateEnd: document.getElementsByName('courseDateEnd')[0].value,
+      coursePlace: document.getElementsByName('coursePlace')[0].value,
+    })
+  }
+
+  getResume () {
+    console.log(this.resume)
+  }
+}
+
+const resume = new ResumeBuilder()
+
+function editCurrentResume() {
+  window.localStorage.setItem('collection', JSON.stringify({
+    resume_id: new URLSearchParams(window.location.search).get('resume_id')
+  }))
+  console.log(JSON.parse(window.localStorage.collection))
+}
+
+function imageUploaded() {
+  const file = document.querySelector('input[type=file]').files[0]
+
+  let reader = new FileReader()
+  reader.addEventListener('load', () => {
+    resume.setPhoto(reader.result)
+  })
+
+  if (file) {
+    reader.readAsDataURL(file)
+  }
+}
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  // let controller = new ResumeController()
-
-  document.body.appendChild(document.getElementsByName('resume')[0].content.cloneNode(true))
+  // const collection = [
+  //   {
+  //     id: self.crypto.randomUUID(),
+  //     photo: ,
+  //     fullname: ,
+  //     birthdate: ,
+  //     residence: ,
+  //     phone: ,
+  //     email: ,
+  //     interests: [
+  //       {
+          // interest: ,
+  //       },
+  //     ],
+  //     languages: [
+  //       {
+          // languageName: ,
+          // languageLevel: ,
+  //       },
+  //     ],
+  //     personalDescription: ,
+  //     jobs: [
+  //       {
+          // jobTitle: ,
+          // jobDateStart: ,
+          // jobDateEnd: ,
+          // jobPlace: ,
+          // jobDescription: ,
+  //       },
+  //     ],
+  //     educations: [
+  //       {
+          // educationTitle: ,
+          // educationDateStart: ,
+          // educationDateEnd: ,
+          // educationPlace: ,
+          // educationDescription: ,
+  //       },
+  //     ],
+  //     courses: [
+  //       {
+          // courseTitle: ,
+          // courseDateStart: ,
+          // courseDateEnd: ,
+          // coursePlace: ,
+  //       },
+  //     ],
+  //   },
+  // ]
 })
-
-// class Collection {
-//   collection = []
-
-//   constructor () {
-
-//   }
-
-//   push (portfolio) {
-//     return this.collection.push(portfolio)
-//   }
-// }
-
-// class Portfolio {
-//   #data = {
-
-//   }
-
-//   constructor () {
-
-//   }
-// }
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   // collection.push(portfolio)
-//   // collection.push(portfolio)
-//   // collection.push(portfolio)
-//   // collection.push(portfolio)
-
-//   // console.log(collection)
-
-//   // collection.forEach((portfolio) => {
-//   //   console.log(portfolio)
-
-//   //   let element = document.createElement('P')
-//   //   element.innerHTML = 'text'
-
-//   //   document.body.append(element)
-//   // })
-
-//   let collection = new Collection()
-
-//   let portfolio = new Portfolio()
-//   collection.push(portfolio)
-//   collection.push(portfolio)
-//   collection.push(portfolio)
-//   collection.push(portfolio)
-//   collection.push(portfolio)
-
-//   console.log(collection)
-// })
