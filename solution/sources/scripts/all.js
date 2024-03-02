ca8eadeb7e12d341e00505b8fc02bb75e8e51b93
Denis Skvortsov <diayuokaro@gmail.com>
2024-03-02 14:24:00 +0300

    Update

--- solution/sources/scripts/all.js
+++ solution/sources/scripts/all.js
@@ -1,5 +1,46 @@
 'use strict'
 
-document.addEventListener('DOMContentLoaded', (event) => {
+function removeResume(resumeId) {
+  const collection = JSON.parse(window.localStorage.getItem('collection'))
+  collection.splice(JSON.parse(window.localStorage.getItem('collection'))?.findIndex((resume) => resume.id === resumeId), 1)
+  window.localStorage.setItem('collection', JSON.stringify(collection))
 
+  refreshItems()
+}
+
+function refreshItems() {
+  const items = document.getElementById('items')
+  items.innerHTML = ''
+
+  JSON.parse(window.localStorage.getItem('collection'))?.forEach((resume) => {
+    items.innerHTML += `
+      <div class="collection_card-wrapper" tabindex="0" title="Действия">
+        <div class="collection_card">
+          <section>
+            <img class="collection_card__photo" src="${ resume.photo || '/sources/pictures/photo.jpg' }">
+          </section>
+          <section>
+            <h3 class="collection_card__fullname">${ resume.title || resume.fullname }</h3>
+            <p class="collection_card__extra">${ resume.residence }</p>
+          </section>
+        </div>
+        <div class="collection_card_options">
+          <a href="/?resume_id=${ resume.id }" class="collection_card_options__button">Открыть</a>
+          <a class="collection_card_options__button" onclick="removeResume('${ resume.id }')">Удалить</a>
+        </div>
+      </div>
+    `
+  })
+
+  items.innerHTML += `
+    <a href="/?new_resume=1" class="collection_card-wrapper" title="Добавить">
+      <div class="collection_card add-plug">
+        <span class="material-symbols-rounded icon">add</span>
+      </div>
+    </a>
+  `
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  refreshItems()
 })
--- solution/sources/scripts/main.js
+++ solution/sources/scripts/main.js
@@ -1,24 +1,83 @@
 'use strict'
 
-class ResumeBuilder {
-  resume = {
-    interests: [],
-    languages: [],
-    jobs: [],
-    educations: [],
-    courses: [],
+class Resume {
+  reference = {
+    // id: ,
+    // title: ,
+    // photo: ,
+    // fullname: ,
+    // birthdate: ,
+    // residence: ,
+    // phone: ,
+    // email: ,
+    interests: [
+      // {
+      //   interest: ,
+      // },
+    ],
+    languages: [
+      // {
+      //   languageName: ,
+      //   languageLevel: ,
+      // },
+    ],
+    // personalDescription: ,
+    jobs: [
+      // {
+      //   jobTitle: ,
+      //   jobDateStart: ,
+      //   jobDateEnd: ,
+      //   jobPlace: ,
+      //   jobDescription: ,
+      // },
+    ],
+    educations: [
+      // {
+      //   educationTitle: ,
+      //   educationDateStart: ,
+      //   educationDateEnd: ,
+      //   educationPlace: ,
+      //   educationDescription: ,
+      // },
+    ],
+    courses: [
+      // {
+      //   courseTitle: ,
+      //   courseDateStart: ,
+      //   courseDateEnd: ,
+      //   coursePlace: ,
+      // },
+    ],
   }
 
-  constructor () {
-    this.resume.id = self.crypto.randomUUID()
+  constructor (resumeId) {
+    if (!window.localStorage.getItem('collection')) {
+      window.localStorage.setItem('collection', JSON.stringify([]))
+    }
+
+    if (resumeId) {
+      const resume = JSON.parse(window.localStorage.getItem('collection'))?.filter((resume) => resume.id === resumeId)
+      if (resume.length) this.reference = resume[0]
+
+      return
+    }
+
+    this.reference.id = self.crypto.randomUUID()
   }
 
-  setPhoto (event) {
-    const file = document.querySelector('input[type=file]').files[0]
+  setPhoto () {
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    const file = resumeForm.get('photo')
+    if (!file.name) return
+    if (file.size > 256*1024) {
+      document.getElementsByName('photo')[0].value = ''
+
+      return
+    }
 
     const reader = new FileReader()
     reader.addEventListener('load', (event) => {
-      this.resume.photo = reader.result
+      this.reference.photo = reader.result
     })
 
     if (file) {
@@ -26,76 +85,196 @@ class ResumeBuilder {
     }
   }
 
-  setFullname (event) {
-    this.resume.fullname = event.target.value
-  }
-
-  setBirthdate (event) {
-    this.resume.birthdate = new Date(event.target.value)
-  }
-
-  setResidence (event) {
-    this.resume.residence = event.target.value
-  }
-
-  setPhone (event) {
-    this.resume.phone = event.target.value
-  }
-
-  setEmail (event) {
-    this.resume.email = event.target.value
-  }
-
   addInterest () {
-    this.resume.interests.push({
-      interest: document.getElementsByName('interest')[0].value,
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('interest')) return
+
+    document.getElementsByName('interest')[0].value = ''
+
+    document.getElementsByName('interests')[0].innerHTML += `<li>
+      <article>
+        <p>${ resumeForm.get('interest') }</p>
+      </article>
+    </li>`
+
+    this.reference.interests.push({
+      interest: resumeForm.get('interest'),
     })
+  }
+
+  removeInterest () {
+    this.reference.interests.pop()
+
+    const node = document.getElementsByName('interests')[0]
+    node.removeChild(node.lastChild)
   }
 
   addLanguage () {
-    this.resume.languages.push({
-      languageName: document.getElementsByName('languageName')[0].value,
-      languageLevel: document.getElementsByName('languageLevel')[0].value,
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('languageName')) return
+    if (!resumeForm.get('languageLevel')) return
+
+    document.getElementsByName('languageName')[0].value = ''
+    document.getElementsByName('languageLevel')[0].value = ''
+
+    document.getElementsByName('languages')[0].innerHTML += `<li>
+      <article>
+        <div>
+          <p>${ resumeForm.get('languageName') }</p>
+          <p>${ resumeForm.get('languageLevel') }</p>
+        </div>
+      </article>
+    </li>`
+
+    this.reference.languages.push({
+      languageName: resumeForm.get('languageName'),
+      languageLevel: resumeForm.get('languageLevel'),
     })
   }
 
-  setPersonalDescription (personalDescription) {
-    this.resume.personalDescription = personalDescription.target.value
+  removeLanguage () {
+    this.reference.languages.pop()
+
+    const node = document.getElementsByName('languages')[0]
+    node.removeChild(node.lastChild)
   }
 
   addJob () {
-    this.resume.jobs.push({
-      jobTitle: document.getElementsByName('jobTitle')[0].value,
-      jobDateStart: document.getElementsByName('jobDateStart')[0].value,
-      jobDateEnd: document.getElementsByName('jobDateEnd')[0].value,
-      jobPlace: document.getElementsByName('jobPlace')[0].value,
-      jobDescription: document.getElementsByName('jobDescription')[0].value,
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('jobTitle')) return
+
+    document.getElementsByName('jobTitle')[0].value = ''
+    document.getElementsByName('jobDateStart')[0].value = ''
+    document.getElementsByName('jobDateEnd')[0].value = ''
+    document.getElementsByName('jobPlace')[0].value = ''
+    document.getElementsByName('jobDescription')[0].value = ''
+
+    document.getElementsByName('jobs')[0].innerHTML += `<li>
+      <article>
+        <p>${ resumeForm.get('jobTitle') }</p>
+        <div>
+          <p>${ resumeForm.get('jobDateStart') }</p>
+          <p>${ resumeForm.get('jobDateEnd') }</p>
+        </div>
+        <p>${ resumeForm.get('jobPlace') }</p>
+        <p>${ resumeForm.get('jobDescription') }</p>
+      </article>
+    </li>`
+
+    this.reference.jobs.push({
+      jobTitle: resumeForm.get('jobTitle'),
+      jobDateStart: resumeForm.get('jobDateStart'),
+      jobDateEnd: resumeForm.get('jobDateEnd'),
+      jobPlace: resumeForm.get('jobPlace'),
+      jobDescription: resumeForm.get('jobDescription'),
     })
+  }
+
+  removeJob () {
+    this.reference.jobs.pop()
+
+    const node = document.getElementsByName('jobs')[0]
+    node.removeChild(node.lastChild)
   }
 
   addEducation () {
-    this.resume.educations.push({
-      educationTitle: document.getElementsByName('educationTitle')[0].value,
-      educationDateStart: document.getElementsByName('educationDateStart')[0].value,
-      educationDateEnd: document.getElementsByName('educationDateEnd')[0].value,
-      educationPlace: document.getElementsByName('educationPlace')[0].value,
-      educationDescription: document.getElementsByName('educationDescription')[0].value,
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('educationTitle')) return
+
+    document.getElementsByName('educationTitle')[0].value = ''
+    document.getElementsByName('educationDateStart')[0].value = ''
+    document.getElementsByName('educationDateEnd')[0].value = ''
+    document.getElementsByName('educationPlace')[0].value = ''
+    document.getElementsByName('educationDescription')[0].value = ''
+
+    document.getElementsByName('educations')[0].innerHTML += `<li>
+      <article>
+        <p>${ resumeForm.get('educationTitle') }</p>
+        <div>
+          <p>${ resumeForm.get('educationDateStart') }</p>
+          <p>${ resumeForm.get('educationDateEnd') }</p>
+        </div>
+        <p>${ resumeForm.get('educationPlace') }</p>
+        <p>${ resumeForm.get('educationDescription') }</p>
+      </article>
+    </li>`
+
+    this.reference.educations.push({
+      educationTitle: resumeForm.get('educationTitle'),
+      educationDateStart: resumeForm.get('educationDateStart'),
+      educationDateEnd: resumeForm.get('educationDateEnd'),
+      educationPlace: resumeForm.get('educationPlace'),
+      educationDescription: resumeForm.get('educationDescription'),
     })
+  }
+
+  removeEducation () {
+    this.reference.educations.pop()
+
+    const node = document.getElementsByName('educations')[0]
+    node.removeChild(node.lastChild)
   }
 
   addCourse () {
-    this.resume.courses.push({
-      courseTitle: document.getElementsByName('courseTitle')[0].value,
-      courseDateStart: document.getElementsByName('courseDateStart')[0].value,
-      courseDateEnd: document.getElementsByName('courseDateEnd')[0].value,
-      coursePlace: document.getElementsByName('coursePlace')[0].value,
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('courseTitle')) return
+
+    document.getElementsByName('courseTitle')[0].value = ''
+    document.getElementsByName('courseDateStart')[0].value = ''
+    document.getElementsByName('courseDateEnd')[0].value = ''
+    document.getElementsByName('coursePlace')[0].value = ''
+
+    document.getElementsByName('courses')[0].innerHTML += `<li>
+      <article>
+        <p>${ resumeForm.get('courseTitle') }</p>
+        <div>
+          <p>${ resumeForm.get('courseDateStart') }</p>
+          <p>${ resumeForm.get('courseDateEnd') }</p>
+        </div>
+        <p>${ resumeForm.get('coursePlace') }</p>
+      </article>
+    </li>`
+
+    this.reference.courses.push({
+      courseTitle: resumeForm.get('courseTitle'),
+      courseDateStart: resumeForm.get('courseDateStart'),
+      courseDateEnd: resumeForm.get('courseDateEnd'),
+      coursePlace: resumeForm.get('coursePlace'),
     })
   }
 
-  getResume () {
-    console.log(this.resume)
+  removeCourse () {
+    this.reference.courses.pop()
+
+    const node = document.getElementsByName('courses')[0]
+    node.removeChild(node.lastChild)
+  }
+
+  generateResume () {
+    const resumeForm = new FormData(document.getElementById('resumeForm'))
+    if (!resumeForm.get('fullname')) return
+
+    this.reference = {
+      ...this.reference,
+      title: resumeForm.get('title'),
+      fullname: resumeForm.get('fullname'),
+      birthdate: resumeForm.get('birthdate'),
+      residence: resumeForm.get('residence'),
+      phone: resumeForm.get('phone'),
+      email: resumeForm.get('email'),
+      personalDescription: resumeForm.get('personalDescription'),
+    }
+
+    if (!JSON.parse(window.localStorage.getItem('collection'))?.filter((resume) => resume.id === this.reference.id).length) {
+      window.localStorage.setItem('collection', JSON.stringify([
+        ...JSON.parse(window.localStorage.getItem('collection')),
+        this.reference,
+      ]))
+    }
   }
 }
+
+const resume = new Resume(new URLSearchParams(window.location.search).get('resume_id'))
 
 // function editCurrentResume() {
 //   window.localStorage.setItem('collection', JSON.stringify({
@@ -104,56 +283,8 @@ class ResumeBuilder {
 //   console.log(JSON.parse(window.localStorage.collection))
 // }
 
-const resume = new ResumeBuilder()
-
 document.addEventListener('DOMContentLoaded', (event) => {
-  // const collection = [
-  //   {
-  //     id: self.crypto.randomUUID(),
-  //     photo: ,
-  //     fullname: ,
-  //     birthdate: ,
-  //     residence: ,
-  //     phone: ,
-  //     email: ,
-  //     interests: [
-  //       {
-          // interest: ,
-  //       },
-  //     ],
-  //     languages: [
-  //       {
-          // languageName: ,
-          // languageLevel: ,
-  //       },
-  //     ],
-  //     personalDescription: ,
-  //     jobs: [
-  //       {
-          // jobTitle: ,
-          // jobDateStart: ,
-          // jobDateEnd: ,
-          // jobPlace: ,
-          // jobDescription: ,
-  //       },
-  //     ],
-  //     educations: [
-  //       {
-          // educationTitle: ,
-          // educationDateStart: ,
-          // educationDateEnd: ,
-          // educationPlace: ,
-          // educationDescription: ,
-  //       },
-  //     ],
-  //     courses: [
-  //       {
-          // courseTitle: ,
-          // courseDateStart: ,
-          // courseDateEnd: ,
-          // coursePlace: ,
-  //       },
-  //     ],
-  //   },
-  // ]
+  // document.getElementsByName('generateResume')[0].addEventListener('click', () => {
+  //   console.log(new FormData(document.getElementById('resumeForm')).get('interest'))
+  // })
 })
